ed6469b7f1c4a01b2286bd4df16e63fe490088a1
Unknown <vitto.leandro@gmail.com>
2018-10-02 17:22:27 +0200

    Login e User Model Update for Jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,17 +9,13 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests\RegisterRequest;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
-    //
-
-    public function login(Request $request){
-        return response(['status' => 'success']);
-    }
 
     public function register(RegisterRequest $request){
-        
+
         $user = new User;
         $user->email = $request->email;
         $user->name = $request->name;
@@ -32,4 +28,43 @@ class AuthController extends Controller
         ], 200);
 
     }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response([
+                    'status' => 'error',
+                    'error' => 'invalid.credentials',
+                    'msg' => 'Invalid Credentials.'
+                ], 401);
+            }
+            /*
+            //Creating a Token based on a user object
+
+            $customClaims = ['foo' => 'bar', 'baz' => 'bob'];
+                JWTAuth::attempt($credentials, $customClaims);
+                // or
+                JWTAuth::fromUser($user, $customClaims);
+            //----------------------------------------------------
+            //Creating a Token based on anything you like
+
+            $customClaims = ['foo' => 'bar', 'baz' => 'bob'];
+            $payload = JWTFactory::make($customClaims);
+            $token = JWTAuth::encode($payload);
+            */
+        }catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response([
+            'status' => 'success'
+        ])
+        ->header('Authorization', $token);
+    }
+
 }
